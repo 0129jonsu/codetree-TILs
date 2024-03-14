deb2035f7fc4ae8e07eb70280d0115ba0417b6ad
JunSu Kim <85074308+0129jonsu@users.noreply.github.com>
2024-03-14 12:51:15 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240314/그 요일은/the-day-of-the-day.cpp
+++ 240314/그 요일은/the-day-of-the-day.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int arr[13] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
-int ans= -1;
+int ans= 0;
 int main() {
     int m1, m2, d1, d2;
+    string mon = "";
     cin >> m1 >> d1 >> m2 >> d2;
+    cin >> mon;
 
     while(m1 != m2 || d1 != d2){
         ans++;
