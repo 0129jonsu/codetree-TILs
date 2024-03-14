16c0e11d78582805caa199972d256ad4778ba85b
JunSu Kim <85074308+0129jonsu@users.noreply.github.com>
2024-03-14 12:54:20 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240314/그 요일은/the-day-of-the-day.cpp
+++ 240314/그 요일은/the-day-of-the-day.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 
 int arr[13] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+string months[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
 int ans= 0;
 int main() {
     int m1, m2, d1, d2;
+    int idx = 0;
     string mon = "";
     cin >> m1 >> d1 >> m2 >> d2;
     cin >> mon;
 
     while(m1 != m2 || d1 != d2){
-        ans++;
+        if (months[idx] == mon) ans++;
+        idx++;
+        if(idx == 7) idx = 0;
         if(arr[m1] == d1){
             d1 = 1;
             m1++;
@@ -20,7 +24,6 @@ int main() {
         d1++;
     }
 
-    cout << ans / 7 + 1;
-    
+    cout << ans;
     return 0;
 }
